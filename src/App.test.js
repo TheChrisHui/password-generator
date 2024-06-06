c4eb27970611a7f8cd7a67f17da311fbfbb817ac
TheChrisHui <huipokshingchris@hotmail.com>
2024-06-06 12:46:51 +0800

    Changed App Test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Password/i);
+  const linkElement = screen.getByText(/Password:/i);
   expect(linkElement).toBeInTheDocument();
 });
